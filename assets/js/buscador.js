cceac3a9b54943924b8323968d2121e78b4f07ea
DanteGb13 <angelrengifo140@gmail.com>
2024-05-26 21:35:30 -0400

    A
    mas

---
+++ assets/js/buscador.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const searchForm = document.getElementById("searchForm");
+    const searchInput = document.getElementById("searchInput");
+    const items = document.querySelectorAll('.card'); 
+
+    searchForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const searchTerm = searchInput.value.toLowerCase();
+        
+        items.forEach(item => {
+            const title = item.querySelector('.card-title').textContent.toLowerCase();
+            if (title.includes(searchTerm)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+});
