d666ebfcaff489536f52ae8537bd6bf64be599ef
Angel Rengifo <angelrengifo140@gmail.com>
2024-06-03 12:05:24 -0400

    contacto
    finalizado con local storage

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,1 +1,34 @@
-/*proximamente....*/
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contactoForm");
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        const mensajeData = {
+            nombre: name,
+            correo: email,
+            mensaje: mensaje
+        };
+
+
+        let mensajes = JSON.parse(localStorage.getItem("mensajes")) || [];
+
+
+        mensajes.push(mensajeData);
+
+
+        localStorage.setItem("mensajes", JSON.stringify(mensajes));
+
+    
+        form.reset();
+
+  
+        alert("Mensaje enviado con éxito!");
+    });
+});
+
